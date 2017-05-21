6a0fc6f752c8d3e4c976ae5b552c2367ac512fd6
Ujin <1987oem@gmail.com>
2017-05-21 23:08:25 +0300

    Add files via upload

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,22 @@
-$(function() {
+$(function () {
+    $("#my-menu").mmenu({
+        extensions: ["theme-black", "effect-menu-slide", "pagedim-black", "shadow-page", "fullscreen", "fx-menu-slide"],
+        navbar: {
+            title: '<img src="img/logo.svg" alt="Make Up">'
+        },
+        offCanvas: {
+            position: 'right'
+        }
+    });
 
-	// Custom JS
+    //   Get the API
+    var api = $("#my-menu").data( "mmenu" );
 
+    //   Hook into methods
+    api.bind( "open:finish", function() {
+        $(".hamburger").addClass("is-active");
+    });
+    api.bind( "close:finish", function() {
+        $(".hamburger").removeClass("is-active");
+    });
 });
